35976992bfafd11d524f33a64cde4b20e22a7712
putongyong <117098471+putongyong@users.noreply.github.com>
2024-12-04 11:01:45 +0100

    update: rename modules

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for
-from src.controllers import controllers
+from src.api import controllers
 from src.models import models
 from src.db.session import engine
 
--- src/api/controllers.py
+++ src/api/controllers.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from sqlalchemy.orm import Session
 from src.entities import UserInputEntities
-from src.crud import crud
+from src.repositories import sql_repository
 from src.db.session import get_db
 from src.use_cases import ReturnPairsUseCase
 from pydantic import ValidationError
@@ -29,7 +29,7 @@ def create_user_input():
         input_data = UserInputEntities.RangeInput(**request.json)
         output_data: UserInputEntities.UserInputBase = ReturnPairsUseCase.return_pairs(input_data)
         # Create the user input in the database
-        db_user_input = crud.create_user_input(db, output_data)
+        db_user_input = sql_repository.create_user_input(db, output_data)
         
         # Convert ORM object to dictionary for Pydantic compatibility
         db_user_input_dict = db_user_input.__dict__
@@ -50,7 +50,7 @@ def read_user_input(user_input_id: str):
     Endpoint to fetch a specific user input by its ID.
     """
     db: Session = next(get_db())
-    db_user_input = crud.get_user_input_by_id(db, user_input_id)
+    db_user_input = sql_repository.get_user_input_by_id(db, user_input_id)
 
     if db_user_input is None:
         return jsonify({"error": "User input not found"}), 404
@@ -71,7 +71,7 @@ def read_all_user_inputs():
         offset = int(request.args.get("offset", 0))  # Default offset to 0
         limit = int(request.args.get("limit", 10))   # Default limit to 10
 
-        user_inputs = crud.get_all_user_inputs(db, skip=offset, limit=limit)
+        user_inputs = sql_repository.get_all_user_inputs(db, skip=offset, limit=limit)
 
         # Convert ORM objects to dictionaries compatible with Pydantic
         user_inputs_dicts = [user_input.__dict__ for user_input in user_inputs]
